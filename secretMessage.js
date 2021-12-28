46e70abd9b32a07cd793d74868ad1c8370a06f06
matthewrk18 <matthewrk18@gmail.com>
2021-12-28 17:53:40 -0500

    newProjects

---
+++ secretMessage.js
@@ -0,0 +1,11 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length)
+console.log(secretMessage.pop())
+console.log(secretMessage.length)
+console.log(secretMessage.push('to', 'Program'))
+console.log(secretMessage.length)
+console.log(secretMessage[7] = 'right')
+console.log(secretMessage.shift())
+console.log(secretMessage.unshift('Programming'))
+secretMessage.splice(6,5,'know,')
+console.log(secretMessage.join(' '))
