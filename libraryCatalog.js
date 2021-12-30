238d55281df90a6f53f33ea9a1990bdc4635219b
matthewrk18 <matthewrk18@gmail.com>
2021-12-30 16:46:29 -0500

    newProject

---
+++ libraryCatalog.js
@@ -0,0 +1,93 @@
+class Media {
+    constructor(title) {
+        this._title = title;
+        this._isCheckedOut = false;
+        this._ratings = [];
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get isCheckedOut() {
+        return this._isCheckedOut;
+    }
+
+    get ratings() {
+        return this._ratings;
+    }
+
+    set isCheckedOut(value) {
+        this._isCheckedOut = value;
+    }
+
+    toggleCheckoutStatus() {
+        this.isCheckedOut = !this.isCheckedOut;
+    }
+
+    getAverageRating() {
+        let ratingsSum = this.ratings.reduce((currentSum, rating) =>
+        currentSum + rating, 0);
+        return ratingsSum / this.ratings.length;
+    }
+
+    addRating(value) {
+        if (value >= 1 && value <=5) { 
+            this.ratings.push(value)
+        }else {
+            console.log('Please enter # between 1 and 5')
+        };
+    }
+}
+
+class Book extends Media {
+    constructor(title, author, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get pages() {
+        return this._pages;
+    }
+}
+
+class Movie extends Media {
+    constructor(title, director, runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+
+    get director() {
+        return this._director;
+    }
+
+    get runTime() {
+        return this._runTime;
+    }
+}
+
+const historyOfEverything = new Book('A Short History of Nearly Everything', 'Bill Bryson', 544)
+
+historyOfEverything.toggleCheckoutStatus()
+console.log(historyOfEverything.isCheckedOut)
+
+historyOfEverything.addRating(4)
+historyOfEverything.addRating(5)
+historyOfEverything.addRating(5)
+console.log(historyOfEverything.getAverageRating())
+
+const speed = new Movie('Speed', 'Jan de Bont', 116)
+
+speed.toggleCheckoutStatus()
+console.log(speed.isCheckedOut)
+
+speed.addRating(6)
+speed.addRating(1)
+speed.addRating(5)
+console.log(speed.getAverageRating())
